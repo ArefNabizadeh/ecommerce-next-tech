aa43250b266498ecdc35fb37d08b1680b47f5095
Aref <arefnabizadehdev@gmail.com>
2026-08-21 18:54:51 +0330

    Update ALLOWED_HOSTS to allow all hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [  "ArefNabizadeh.pythonanywhere.com",]
+ALLOWED_HOSTS = [  "*",]
 
 
 
